c8439efdba44938c960bf3794dbb611175c2bc3b
Roberto Camillo <robertodesouzacamillo@gmail.com>
2025-02-04 15:17:32 -0300

    Bug pesquisa corrigido

--- script.js
+++ script.js
@@ -5,30 +5,48 @@ const resultPlaylist = document.getElementById('result-playlists');
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
+            .then((response) => response.json())
+            .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add('hidden');
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = '';
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
+    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm));
+
+
+    filteredArtists.forEach(artist => {
+        const artistCard = document.createElement('div');
+        artistCard.classList.add('artist-card');
+
+        artistCard.innerHTML = `
+        <div class="card-img">
+            <img class="artist-img" src="${artist.urlImg}" />
+            <div class="play">
+                <span class="fa fa-solid fa-play"></span>
+            </div>
+        </div>
+        <div class="card-text">
+            <span class="artist-name">${artist.name}</span>
+            <span class="artist-categorie">Artista</span>
+        </div>
+    `;
+    gridContainer.appendChild(artistCard);
     });
 
     resultArtist.classList.remove('hidden');
 }
 
-document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
+document.addEventListener('input',function() {
+    const searchTerm = searchInput.value.toLowerCase().trim();
+
     if (searchTerm === '') {
         resultPlaylist.classList.remove('hidden');
         resultArtist.classList.add('hidden');
         return;
     }
-    
+
     requestApi(searchTerm);
-})
+});
