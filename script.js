fd63d3c45719bd7683ff0dd4918750ab6d1994b9
Roberto Camillo <robertodesouzacamillo@gmail.com>
2025-02-04 16:07:03 -0300

    Aula 4 Concluída

--- script.js
+++ script.js
@@ -16,28 +16,35 @@ function displayResults(result, searchTerm) {
 
     const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm));
 
+    // Se houver mais de 4 artistas, adiciona a classe para ajuste do layout
+    if (filteredArtists.length > 4) {
+        gridContainer.classList.add('multi-line'); // Adiciona classe extra para CSS
+    } else {
+        gridContainer.classList.remove('multi-line'); // Remove se não for necessário
+    }
 
     filteredArtists.forEach(artist => {
         const artistCard = document.createElement('div');
         artistCard.classList.add('artist-card');
 
         artistCard.innerHTML = `
-        <div class="card-img">
-            <img class="artist-img" src="${artist.urlImg}" />
-            <div class="play">
-                <span class="fa fa-solid fa-play"></span>
+            <div class="card-img">
+                <img class="artist-img" src="${artist.urlImg}" />
+                <div class="play">
+                    <span class="fa fa-solid fa-play"></span>
+                </div>
             </div>
-        </div>
-        <div class="card-text">
-            <span class="artist-name">${artist.name}</span>
-            <span class="artist-categorie">Artista</span>
-        </div>
-    `;
-    gridContainer.appendChild(artistCard);
+            <div class="card-text">
+                <span class="artist-name">${artist.name}</span>
+                <span class="artist-categorie">Artista</span>
+            </div>
+        `;
+        gridContainer.appendChild(artistCard);
     });
 
     resultArtist.classList.remove('hidden');
 }
+
 
 document.addEventListener('input',function() {
     const searchTerm = searchInput.value.toLowerCase().trim();
